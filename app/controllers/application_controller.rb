12f2d0a465a857f25029e8efd6a1ba5652f45297
StuartFleishman <Stuart.Alex.Fleishman@gmail.com>
2020-12-12 13:01:18 -0500

    enable/set session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,11 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
+  configure do
+    enable :sessions
+    set :session_secret, "secret"
+  end
+
   get "/" do
     erb :welcome
   end
