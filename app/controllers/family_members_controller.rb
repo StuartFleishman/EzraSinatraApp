d80ec05c1eb54efe47ab5c131773d347839cc360
StuartFleishman <Stuart.Alex.Fleishman@gmail.com>
2020-12-12 14:45:58 -0500

    built out index for family members

--- app/controllers/family_members_controller.rb
+++ app/controllers/family_members_controller.rb
@@ -1,12 +1,9 @@
 class FamilyMembersController < ApplicationController 
 
-    get '/signup' do 
-        erb :"users/new"
+    get '/familymembers' do 
+        @familymembers = FamilyMember.all
+        erb :"familymembers/index"
     end 
 
-    post '/signup' do 
-        @user = User.create(params[:user])
-        session[:user_id] = @user.id 
-        erb :'users/welcome' 
-    end 
+
 end 
