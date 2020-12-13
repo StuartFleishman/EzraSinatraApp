dca045eef05b926fa8649955d6b0c5045a2af4d4
StuartFleishman <Stuart.Alex.Fleishman@gmail.com>
2020-12-12 19:01:53 -0500

    adding validating to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base 
     has_secure_password
     has_many :family_members 
+    validates :user_name, presence: true 
+    validates :user_name, uniqueness: true 
 end 
