3b94713249f2aca9a14472a42292dd034381e57e
StuartFleishman <Stuart.Alex.Fleishman@gmail.com>
2020-12-12 19:32:15 -0500

    added in errors to signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,8 +5,13 @@ class UsersController < ApplicationController
     end 
 
     post '/signup' do 
-        @user = User.create(params[:user])
-        session[:user_id] = @user.id 
-        erb :'users/welcome' 
-    end 
+        @user = User.new(params[:user])
+        if @user.save
+            session[:user_id] = @user.id 
+            erb :'users/welcome' 
+        else 
+            @erros = @user.errors.full_messages.join(" - ")    
+            erb :'users/new' 
+        end 
+     end 
 end 
